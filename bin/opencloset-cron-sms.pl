c0ee56f88a3375662bf8e07ed4e4f633acc3b202
Keedi Kim <keedi.k@gmail.com>
2015-07-09 20:57:16 +0900

    opencloset-cron-sms.pl checks config file before execute

--- bin/opencloset-cron-sms.pl
+++ bin/opencloset-cron-sms.pl
@@ -23,14 +23,22 @@ die "Usage: $Script <config path>\n" unless $config_file && -f $config_file;
 
 my $CONF     = OpenCloset::Config::load($config_file);
 my $APP_CONF = $CONF->{$Script};
+my $DB_CONF  = $CONF->{database};
 my $SMS_CONF = $CONF->{sms};
+my $TIMEZONE = $CONF->{timezone};
+
+die "$config_file: $Script is needed\n"    unless $APP_CONF;
+die "$config_file: database is needed\n"   unless $DB_CONF;
+die "$config_file: sms is needed\n"        unless $SMS_CONF;
+die "$config_file: sms.driver is needed\n" unless $SMS_CONF && $SMS_CONF->{driver};
+die "$config_file: timezone is needed\n"   unless $TIMEZONE;
 
 my $DB = OpenCloset::Schema->connect(
     {
-        dsn      => $CONF->{database}{dsn},
-        user     => $CONF->{database}{user},
-        password => $CONF->{database}{pass},
-        %{ $CONF->{database}{opts} },
+        dsn      => $DB_CONF->{dsn},
+        user     => $DB_CONF->{user},
+        password => $DB_CONF->{pass},
+        %{ $DB_CONF->{opts} },
     }
 );
 
@@ -39,7 +47,7 @@ my $worker1 = do {
     $w = OpenCloset::Cron::Worker->new(
         name      => 'notify_1_day_before',
         cron      => '00 11 * * *',
-        time_zone => $CONF->{timezone},
+        time_zone => $TIMEZONE,
         cb        => sub {
             my $name = $w->name;
             my $cron = $w->cron;
@@ -48,7 +56,7 @@ my $worker1 = do {
             #
             # get today datetime
             #
-            my $dt_now = try { DateTime->now( time_zone => $CONF->{timezone} ); };
+            my $dt_now = try { DateTime->now( time_zone => $TIMEZONE ); };
             return unless $dt_now;
 
             my $dt_start = try { $dt_now->clone->truncate( to => 'day' )->add( days => 1 ); };
