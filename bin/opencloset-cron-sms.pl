1eafb6a688d64899f3a48d7b9420175b7e52a2b2
Keedi Kim <keedi.k@gmail.com>
2015-07-10 15:12:17 +0900

    Send sms 2 day after target date or user target date

--- bin/opencloset-cron-sms.pl
+++ bin/opencloset-cron-sms.pl
@@ -123,11 +123,96 @@ my $worker1 = do {
     );
 };
 
+my $worker2 = do {
+    my $w;
+    $w = OpenCloset::Cron::Worker->new(
+        name      => 'notify_2_day_after',
+        cron      => '00 11 * * *',
+        time_zone => $TIMEZONE,
+        cb        => sub {
+            my $name = $w->name;
+            my $cron = $w->cron;
+            AE::log( info => "$name\[$cron] launched" );
+
+            #
+            # get today datetime
+            #
+            my $dt_now = try { DateTime->now( time_zone => $TIMEZONE ); };
+            return unless $dt_now;
+
+            my $dt_start =
+                try { $dt_now->clone->truncate( to => 'day' )->subtract( days => 2 ); };
+            return unless $dt_start;
+
+            my $dt_end = try {
+                $dt_now->clone->truncate( to => 'day' )->subtract( days => 1 )
+                    ->subtract( seconds => 1 );
+            };
+            return unless $dt_end;
+
+            my ( $lquote, $rquote, $sep ) = get_quote();
+
+            my $dtf      = $DB->storage->datetime_parser;
+            my $order_rs = $DB->resultset('Order')->search(
+                {
+                    status_id => 2,
+                    -or       => [
+                        {
+                            # 반납 희망일이 반납 예정일보다 이른 경우
+                            # 반납 예정일 이틀 후 오전 11시에 발송
+                            'target_date' => [
+                                '-and',
+                                \"> ${lquote}user_target_date${rquote}",
+                                { -between => [ $dtf->format_datetime($dt_start), $dtf->format_datetime($dt_end) ] },
+                            ],
+                        },
+                        {
+                            # 반납 희망일과 반납 예정일이 동일한 경우
+                            # 반납 희망일 이틀 후 오전 11시에 발송
+                            'target_date'      => { -ident => 'user_target_date' },
+                            'user_target_date' => {
+                                -between => [ $dtf->format_datetime($dt_start), $dtf->format_datetime($dt_end) ],
+                            },
+                        },
+                        {
+                            # 반납 희망일이 반납 예정일보다 이후인 경우
+                            # 반납 희망일 이틀 후 오전 11시에 발송
+                            'target_date'      => \"< ${lquote}user_target_date${rquote}",
+                            'user_target_date' => {
+                                -between => [ $dtf->format_datetime($dt_start), $dtf->format_datetime($dt_end) ],
+                            },
+                        },
+                    ],
+                },
+                { order_by => { -asc => 'user_target_date' } },
+            );
+
+            while ( my $order = $order_rs->next ) {
+                my $to = $order->user->user_info->phone || q{};
+                my $msg = sprintf(
+                    '[열린옷장] %d일에 대여하신 의류가 반납되지 않았습니다. 빠른 반납 부탁드립니다.',
+                    $order->rental_date->day,
+                );
+
+                my $log = sprintf(
+                    'id(%d), name(%s), phone(%s), rental_date(%s), target_date(%s), user_target_date(%s)',
+                    $order->id, $order->user->name, $to, $order->rental_date, $order->target_date,
+                    $order->user_target_date );
+                AE::log( info => $log );
+
+                send_sms( $to, $msg ) if $to;
+            }
+
+            AE::log( info => "$name\[$cron] finished" );
+        },
+    );
+};
+
 my $cron = OpenCloset::Cron->new(
     aelog   => $APP_CONF->{aelog},
     port    => $APP_CONF->{port},
     delay   => $APP_CONF->{delay},
-    workers => [$worker1],
+    workers => [ $worker1, $worker2 ],
 );
 $cron->start;
 
@@ -145,4 +230,13 @@ sub send_sms {
 
     my %data = ( $sms->get_columns );
     return \%data;
+}
+
+sub get_quote {
+    my $o_rs      = $DB->resultset('Order');
+    my $rsrc      = $o_rs->result_source;
+    my $sql_maker = $rsrc->storage->sql_maker;
+    my ( $lquote, $rquote, $sep ) = ( $sql_maker->_quote_chars, $sql_maker->name_sep );
+
+    return ( $lquote, $rquote, $sep );
 }
